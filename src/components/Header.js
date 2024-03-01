44404e63902552e6a560f029a9699168986bcf3e
R.Mohamed Sabith <sabith6782@gmail.com>
2024-03-02 00:26:14 +0530

    changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,11 +13,11 @@ export default function Header() {
         <a className="font-bold text-black" href="#">R.Mohamed Sabith</a>
         <nav className="hidden md:block">
             <ul className="flex text-white ">
-                <li><a href="/">Home</a></li>
-                <li><a href="/#about">About</a></li>
-                <li><a href="/#projects">Projects</a></li>
-                <li><a href="/#resume">Resume</a></li>
-                <li><a href="/#contact">Contact</a></li>
+                <li><a href="">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#resume">Resume</a></li>
+                <li><a href="#contact">Contact</a></li>
             </ul>
         </nav>
         {toggleMenu && <nav className="block md:hidden ">
